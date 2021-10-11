1a45eda772d36396cca103d87fde2463cbe95263
sumandas49 <sumandasddm@gmail.com>
2021-10-11 21:27:10 +0530

    search invoice added and email service added with invoice link

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,10 +1,13 @@
 const router = require("express").Router()
 const { isAuthenticated, isSignedIn } = require("../controllers/auth")
-const { makeInvoice } = require("../controllers/invoice")
+const { makeInvoice, getInvoiceById, getInvoiceLink, searchInvoiceByCustomersEmail } = require("../controllers/invoice")
 const { getUserById } = require("../controllers/user")
 
 router.param("userId", getUserById)
+router.param("invoiceId", getInvoiceById)
 
 router.post("/add/:userId", isAuthenticated, isSignedIn, makeInvoice)
+router.get("/email/:invoiceId", getInvoiceLink)
+router.post("/search/by/email/:userId", isAuthenticated, isSignedIn, searchInvoiceByCustomersEmail)
 
 module.exports = router
